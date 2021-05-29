93d7bcfd3e1ac94282886d4fb51c26e29c5c5fa5
Klim <klim@dev.izibook.ru>
2021-05-29 11:10:04 +0300

    test

--- server.js
+++ server.js
@@ -7,6 +7,21 @@ const io = require('socket.io')(server)
 const product_mode = false
 exports.pMode = product_mode
 
+// const i2c1 = require('i2c-bus')
+
+const raspi = require('raspi')
+const Serial = require('raspi-serial').Serial
+ 
+raspi.init(() => {
+  var serial = new Serial()
+  serial.open(() => {
+    serial.on('data', (data) => {
+      console.log(data)
+    });
+    console.log('Hello from raspi-serial');
+  });
+});
+
 //Шаблонизатор и статика
 app.set('views', './templates')
 app.set('view engine', 'pug')
